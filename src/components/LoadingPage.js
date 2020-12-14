b8827adbfba3385c005422b65fca4d5b7a271f1a
Thomas Pappas <thpapei1@gmail.com>
2020-12-14 23:24:26 +0200

    Created a loading page

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,11 @@
+import loading from '../images/loading.gif'
+
+const LoadingPage = () => {
+  return (
+    <div className='loading-page'>
+      <img src={loading} alt='loading.gif' />
+    </div>
+  );
+}
+
+export default LoadingPage;
